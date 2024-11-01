657215953c2f5a8bd094c851e5639c06e66ebc4d
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-11-01 13:51:37 -0400

    chore:minor fix to package.json

--- algeblitz/apps/expo/app/index.tsx
+++ algeblitz/apps/expo/app/index.tsx
@@ -11,13 +11,6 @@ export default function Screen() {
         }}
       />
       <HomeScreen />
-      {/*
-      <Stack.Screen
-        options={{
-          title : 
-        }}
-      
-      />*/}
     </Stack>
   )
 }
