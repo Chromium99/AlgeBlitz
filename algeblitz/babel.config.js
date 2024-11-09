b525033f8df6ed87abcfcf08e60ad4c4694ad070
wong-ja <wongjuanaa@gmail.com>
2024-11-08 19:09:22 -0500

    fix linter issues

--- algeblitz/babel.config.js
+++ algeblitz/babel.config.js
@@ -10,10 +10,10 @@ module.exports = function (api) {
           root: ['.'],
           extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
           alias: {
-            '@': './src',
-          },
-        },
-      ],
-    ],
+            '@': './src'
+          }
+        }
+      ]
+    ]
   }
 }
