ac663e99012a66f14d251a908c3ff7eb6f47417e
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-11-11 19:21:02 -0500

    fix:PR fix

--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
 export const db = new PrismaClient();
-const prisma = new PrismaClient();
 
 //@see https://www.prisma.io/docs/orm/prisma-client/setup-and-configuration/introduction#3-importing-prisma-client
