d546b4c88d9d9bec561f33944afb5809bb1d5a8d
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-11-14 18:15:34 -0500

    feat:fixed fonts, added custom fonts using tailwindcss

--- client/apps/expo/app/_layout.tsx
+++ client/apps/expo/app/_layout.tsx
@@ -3,7 +3,8 @@ import { useEffect } from 'react'
 import { useColorScheme } from 'react-native'
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { useFonts } from 'expo-font'
-import { SplashScreen, Stack } from 'expo-router'
+import { Stack } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
 import { Provider } from 'app/provider'
 import { NativeToast } from '@my/ui/src/NativeToast'
 import '../global.css'
@@ -14,24 +15,34 @@ export const unstable_settings = {
 }
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync()
+//SplashScreen.preventAutoHideAsync()
 
 export default function App() {
-  const [interLoaded, interError] = useFonts({
-    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
-    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  /* const [interLoaded, interError] = useFonts({
+    // TODO : Fix this, fonts not loading correctly.
+    //InterItalic: require('client/apps/expo/fonts/Inter-Italic-VariableFont_opsz,wght.ttf'),
+    //rInterVariable: require('client/apps/expo/fonts/Inter-VariableFont_opsz,wght.ttf'),
+    ItimRegular: require('/Users/ayandas/Desktop/VS_Code_Projects/Alge-Blitz/client/apps/expo/fonts/Itim-Regular.ttf'),
+    // may require their paths to be hardcoded
+    //Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    //InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
   })
 
   useEffect(() => {
     if (interLoaded || interError) {
       // Hide the splash screen after the fonts have loaded (or an error was returned) and the UI is ready.
-      SplashScreen.hideAsync()
+      console.log(interError)
+      console.log(interLoaded)
+      console.log('Error occuring here!')
+      //SplashScreen.hideAsync()
+    } else {
+      console.log('Font loaded successfully!')
     }
   }, [interLoaded, interError])
 
   if (!interLoaded && !interError) {
     return null
-  }
+  } */
 
   return <RootLayoutNav />
 }
@@ -44,16 +55,16 @@ function RootLayoutNav() {
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen
+            name="index"
+            options={{
+              title: 'Home',
+            }}
+          />
+          <Stack.Screen
             name="(tabs)"
             options={{
               title: '',
               headerShown: true,
-            }}
-          />
-          <Stack.Screen
-            name="index"
-            options={{
-              title: 'Home',
             }}
           />
           <Stack.Screen
--- client/apps/expo/app/index.tsx
+++ client/apps/expo/app/index.tsx
@@ -1,8 +1,24 @@
 import { HomeScreen } from 'app/features/home/screen'
-import { Stack, router } from 'expo-router'
+import { useRouter } from 'expo-router'
 import { Button, SafeAreaView, Text } from 'react-native'
+//import { useFonts } from 'expo-font'
+import { useEffect } from 'react'
 
 export default function Home() {
+  const router = useRouter()
+  // we will not be using useFonts, we will use tailwindcss instead
+  // locally load and test out the fonts
+  // const [loaded, error] = useFonts({
+  //   test: require('/Users/ayandas/Desktop/VS_Code_Projects/Alge-Blitz/client/apps/expo/fonts/Itim-Regular.ttf'),
+  // })
+
+  // useEffect(() => {
+  //   if (loaded || error) {
+  //     console.log('Error loading font')
+  //   } else if (!error) {
+  //     console.log('Loaded font successfully')
+  //   }
+  // }, [loaded, error])
   return (
     // NOTE : tab views are treated as screens simultaneously (Don't remove)
     /**
@@ -13,8 +29,17 @@ export default function Home() {
      * @SafeAreaView is simply used to ensure content stays within header
      */
     <SafeAreaView>
-      <Text className="text-red-900 mt-20 text-2xl mx-auto">Some Random Text</Text>
+      <Text
+        style={{
+          fontFamily: 'test',
+        }}
+        // font-inter will help you use the inter font
+        className="font-inter text-red-900 mt-20 text-2xl mx-auto"
+      >
+        Some Random Text
+      </Text>
 
+      {/**These errors can be ignored at the moment. */}
       <Button title="main-tab" onPress={() => router.push('(tabs)/')} />
       <Button title="settings-tab" onPress={() => router.push('(tabs)/settings')} />
     </SafeAreaView>
