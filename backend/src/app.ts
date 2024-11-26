52ad2acd56f56e1b62b0a227ee19ba81544db4d3
MystProgrammer <animeshdeb89@gmail.com>
2024-11-25 19:28:41 -0500

    play mode endpoints implemented, schema changes, test file changes, app.ts changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,18 @@
 import getQuestion from "./routes/getQuestion";
 import submitAnswer from "./routes/submitAnswer";
+import playMode from "./routes/playModes";
 import fastify from "fastify";
 const server=fastify()
-
+const PORT=process.env.PORT? parseInt(process.env.PORT, 10):3000;
 
 server.get('/ping', async (request, reply) => {
-  return 'pong\n'
+  return `listening at port ${PORT}`
 })
 server.register(getQuestion)
 server.register(submitAnswer)
+server.register(playMode)
 
-server.listen({ port: 3000 }, (err, address) => {
+server.listen({ port: PORT }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
