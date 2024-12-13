73cce487438f02c6a78e7a884fceed3e3530f9f2
JNikolo <125705821+JNikolo@users.noreply.github.com>
2024-12-13 00:59:10 -0500

    fix: Change default port number to serve the app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,16 @@
 import getQuestion from "./routes/getQuestion";
 import submitAnswer from "./routes/submitAnswer";
 import fastify from "fastify";
-const server=fastify()
-
+const server = fastify();
 
 server.get("/ping", async (request, reply) => {
-  return "pong\n"
-})
-server.register(getQuestion)
-server.register(submitAnswer)
+  return "pong\n";
+});
+server.register(getQuestion);
+server.register(submitAnswer);
 
 server.listen(
-  { port: Number(process.env.PORT) || 3000, host: "0.0.0.0" },
+  { port: Number(process.env.PORT) || 5000, host: "0.0.0.0" },
   (err, address) => {
     if (err) {
       console.error(err);
